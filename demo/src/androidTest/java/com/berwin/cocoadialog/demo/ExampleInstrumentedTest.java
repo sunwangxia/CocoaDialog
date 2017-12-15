dbfc8254a197ec2558a02479200480cbd7a3a30b
berwin <berwin.sun@foxmail.com>
2017-12-15 11:18:06 +0800

    modified InstrumentedTest class

--- demo/src/androidTest/java/com/berwin/cocoadialog/demo/ExampleInstrumentedTest.java
+++ demo/src/androidTest/java/com/berwin/cocoadialog/demo/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.xia.cocoadialog.demo", appContext.getPackageName());
+        assertEquals("com.berwin.cocoadialog.demo", appContext.getPackageName());
     }
 }
